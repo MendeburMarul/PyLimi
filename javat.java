56da0ac81db6b649a25c75eb83b678b52c63f78f
MendeburMarul <67334424+MendeburMarul@users.noreply.github.com>
2020-10-31 11:04:42 +0300

    Create javat.java

---
+++ javat.java
@@ -0,0 +1,16 @@
+
+@OpenFile
+public void givenPythonScript_whenPythonProcessInvoked_thenSuccess() throws Exception {
+    ProcessBuilder processBuilder = new ProcessBuilder("python", resolvePythonScriptPath("main.py"));
+    processBuilder.redirectErrorStream(true);
+ 
+    Process process = processBuilder.start();
+    List<String> results = readProcessOutput(process.getInputStream());
+ 
+    assertThat("Results should not be empty", results, is(not(empty())));
+    assertThat("Results should contain output of script: ", results, hasItem(
+      containsString("MendeburMarul")));
+ 
+    int exitCode = process.waitFor();
+    assertEquals("No errors should be detected", 0, exitCode);
+}
